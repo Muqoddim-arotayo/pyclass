c65f966f067d71060303f5f5455531e8eb55ee55
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-10 20:34:46 +0100

    added some extra features to the class file

--- Personal_practice.py/class.py
+++ Personal_practice.py/class.py
@@ -2,7 +2,17 @@ print("OOP - class")
 # Class is an object constructor, it is a blue print for our objects
 
 class Myclass :
+
+    # In Python, the __init__ method is a special built-in method (often called a constructor) that’s automatically called when you create a new object from a class.
+
+    def __init__(self, name, number_of_student, class_teacher):
+        self.name = name
+        self.number_of_student = number_of_student
+        self.class_teacher = class_teacher
+
     x = 5
 
-first_class = Myclass
-print(first_class.x)
+first_class = Myclass("300 level","78","Miss opatola")
+second_class = Myclass("200 level","30","Mister oladipupo")
+print(first_class.class_teacher)
+print(second_class.class_teacher)
