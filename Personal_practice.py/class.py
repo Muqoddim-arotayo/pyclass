47f282abae737af3a2af3bb0d155497ca19ee7c0
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-10 22:39:06 +0100

    added a new iterator class file

--- Personal_practice.py/class.py
+++ Personal_practice.py/class.py
@@ -1,18 +1,57 @@
 print("OOP - class")
 # Class is an object constructor, it is a blue print for our objects
 
-class Myclass :
+class Classroom :
 
-    # In Python, the __init__ method is a special built-in method (often called a constructor) that’s automatically called when you create a new object from a class.
+    # Class Attributes → Defined outside __init__
+    # - Shared across all instances of the class.
+    # - Belong to the class itself, not individual objects.
+    # - Usually defined directly in the class body, outside any method.
 
-    def __init__(self, name, number_of_student, class_teacher):
+    school_name = "Afe Babalola University"
+
+
+    # the __init__ method is a special built-in method (often called a constructor) that’s automatically called when you create a new object from a class.
+
+    # Instance Attributes → Usually defined in __init__
+    # - Belong to a specific object (instance) of the class.
+    # - Each instance gets its own copy.
+    # - Usually set inside __init__ using self.attribute.
+
+    def __init__(self, name, number_of_student, class_teacher, student_names = None):
+        #  The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.
+        #  It doesnt have to be self, it can be anything
         self.name = name
         self.number_of_student = number_of_student
         self.class_teacher = class_teacher
+        self.student_names = student_names if student_names is not None else []
 
-    x = 5
+    # The __str__ is a special method that controls what’s returned when you turn an object into a string — for example, when you use print(obj) or str(obj).
 
-first_class = Myclass("300 level","78","Miss opatola")
-second_class = Myclass("200 level","30","Mister oladipupo")
-print(first_class.class_teacher)
-print(second_class.class_teacher)
+    def __str__(self):
+        return f"This is class {self.name}, with {self.number_of_student} students"
+    
+    def add_student(self,add_student_name):
+        self.student_names.append(add_student_name)
+    
+
+    # Defining a method inside of a class
+
+    def learn(self, subject_being_learnt):
+        print(f"We are leaening {subject_being_learnt}")
+
+
+first_class = Classroom(name="300 level",number_of_student="78",class_teacher="Miss opatola",student_names=(["muqoddim","eniola"]))
+second_class = Classroom(name="200 level",number_of_student="30",class_teacher="Mister oladipupo")
+first_class.add_student("ayomide")
+print(first_class.student_names)
+print(second_class.student_names)
+
+# Delete object properties by using the del keyword
+del first_class.student_names
+print(first_class.student_names)
+
+# Delete an object by using the del keyword
+del first_class
+print(first_class)
+
--- Personal_practice.py/inheritance.py
+++ Personal_practice.py/inheritance.py
@@ -3,3 +3,27 @@
 # Parent class is the class being inherited from, also called base class.
 # Child class is the class that inherits from another class, also called derived class.
 
+
+class Parent:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(f"Hello, my name is {self.name}")
+
+    def walk(self):
+        pass # Use the pass keyword when you do not want to add any other properties or methods to the class.
+
+class Child(Parent):
+    def __init__(self, name):
+        self.name = name
+
+    def play(self):
+        print("hey watch me play")
+
+first_child = Child("daniel")
+first_child.speak()
+first_child.play()
+
+first_parent = Parent("vivian")
+first_parent.speak()
