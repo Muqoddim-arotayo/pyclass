d27afa5782ef4ae782e27516708addb2b0163c3f
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-12 11:22:26 +0100

    added a new scope class file

--- Personal_practice.py/iterators.py
+++ Personal_practice.py/iterators.py
@@ -1,1 +1,38 @@
 print("Iterators")
+
+# An iterator is an object that contains a countable numbers of values.
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+# my_list = ["muqoddim","eniola","deborah"]
+# my_it = iter(my_list)
+
+# print(next(my_it))
+
+class Iterator():
+    def __init__(self, parameter):
+        self.current = 0
+        self.parameter = parameter
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current < len(self.parameter):
+            char = self.parameter[self.current]
+            self.current += 1
+            return char
+        else :
+            raise StopIteration
+        
+my_iter = Iterator("muqoddim")
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+
+        
